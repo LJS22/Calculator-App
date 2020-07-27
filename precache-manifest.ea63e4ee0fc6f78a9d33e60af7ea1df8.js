26f7f4c1e275fa1aea822b4e2fe8056e64b9ba4b
LJS22 <LJS22@users.noreply.github.com>
2020-07-27 13:32:24 +0100

    Updates

---
+++ precache-manifest.ea63e4ee0fc6f78a9d33e60af7ea1df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1046d8dd4fc4fa0aed646f54316c2b1",
+    "url": "/Calculator-App/index.html"
+  },
+  {
+    "revision": "ba34fa5cc6da69e3eb85",
+    "url": "/Calculator-App/static/css/main.6d66fce7.chunk.css"
+  },
+  {
+    "revision": "3d8e6da27f95c2e84fc6",
+    "url": "/Calculator-App/static/js/2.25e20656.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-App/static/js/2.25e20656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba34fa5cc6da69e3eb85",
+    "url": "/Calculator-App/static/js/main.2266dffb.chunk.js"
+  },
+  {
+    "revision": "b6a540385bdd88408a89",
+    "url": "/Calculator-App/static/js/runtime-main.8f9141a2.js"
+  }
+]);
